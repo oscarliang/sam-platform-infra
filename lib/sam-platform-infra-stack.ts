43aa7c69812e16fb2f7b19198cd16214d09f9020
heshan-symplicit <157882527+heshan-symplicit@users.noreply.github.com>
2024-05-30 22:01:44 +0530

    Add non prod env (#5)
    * Add APIG
    
    * add vpc link
    
    * Add VPC links
    
    * Add intergration
    
    * updates
    
    * update gitignore
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * add user group
    
    * add ac workers
    
    * add user groups
    
    * Add cognito user pool
    
    * updates
    
    * Add redis
    
    * updates
    
    * updates
    
    * updates
    
    * Add RDS
    
    * updates RDS configuration
    
    * Revert "updates RDS configuration"
    
    This reverts commit beaa7a686f0c921f5edd35f4f9cb2234de32fb9e.
    
    * EKS cluster
    
    * updates
    
    * Add user pools
    
    * Redis
    
    * Updates
    
    * Updates
    
    * optimization

--- lib/sam-platform-infra-stack.ts
+++ lib/sam-platform-infra-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
-import { aws_ec2 as ec2, aws_eks as eks, aws_iam as iam, aws_rds as rds, aws_secretsmanager as secretsmanager} from 'aws-cdk-lib';
+import { aws_ec2 as ec2, aws_eks as eks, aws_iam as iam, aws_rds as rds, aws_secretsmanager as secretsmanager, aws_apigatewayv2 as apigateway, aws_apigatewayv2_integrations as apig_int, aws_cognito as cognito, aws_elasticache as elasticache} from 'aws-cdk-lib';
 import { InstanceType } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
@@ -14,8 +14,19 @@ export class SamPlatformInfraStack extends cdk.Stack {
     let config;
 
     try {
-      const env = process.env.NODE_ENV || 'development';
-      const configFile = env === 'production' ? './config.prod.yaml' : './config.dev.yaml';
+      const env = process.env.NODE_ENV;
+      let configFile;
+
+      switch (env) {
+        case 'production':
+          configFile = './config.prod.yaml';
+          break;
+        case 'nonprod':
+          configFile = './config.nonprod.yaml';
+          break;
+        default:
+          configFile = './config.dev.yaml';
+      }
 
       const fileContents = fs.readFileSync(configFile, 'utf8');
       config = yaml.load(fileContents);
@@ -153,6 +164,184 @@ export class SamPlatformInfraStack extends cdk.Stack {
       nodeRole: eksNodeRole,
       subnets: {subnets: vpc.privateSubnets,},
     });
+
+    const apigConfig = config.APIG;
+
+    // Create the HTTP APIG
+    const httpApi = new apigateway.HttpApi(this, 'MyHttpApi', {
+      apiName: apigConfig.name,
+      createDefaultStage: false
+    });
+
+    // Create the custom stage
+    new apigateway.HttpStage(this, 'MyStage', {
+      httpApi: httpApi,
+      stageName: apigConfig.stageName,
+      autoDeploy: true
+    });    
+
+    // Create the security group for the VPC link
+    const securityGroup = new ec2.SecurityGroup(this, 'VPCLinkSecurityGroup', {
+      vpc,
+      securityGroupName: 'vpc-link',
+      description: 'Security group for API Gateway VPC link',
+    });
+    // Add egress rule
+    securityGroup.addEgressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.allTraffic(),
+      'Allow all outbound traffic'
+    );
+
+    // Create a VPC link for API Gateway
+    const vpcLink = new apigateway.VpcLink(this, 'EksVpcLink', {
+      vpcLinkName: 'eks',
+      vpc,
+      subnets: {
+        subnets: vpc.privateSubnets
+      },
+      securityGroups: [securityGroup]
+    });
+
+    // ********************** Cognito User Pool *****************************
+    const cognitoConfig = config.COGNITO;
+
+    const userPool = new cognito.UserPool(this, 'samMain', {
+      userPoolName: 'sam-main',
+      selfSignUpEnabled: true,
+      deletionProtection: true,
+      userVerification: {
+        emailSubject: 'Your verification code',
+        emailBody: 'Your verification code is {####}.',
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+      },
+      signInAliases: {
+        email: true,
+        username: true,
+        phone: true,
+      },
+      autoVerify: { email: true },
+      keepOriginal: {email: true},
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: true,
+        },
+        givenName: {
+          required: true,
+          mutable: true,
+        },
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireDigits: true,
+        requireLowercase: true,
+        requireUppercase: true,
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+
+      // MFA configurations
+      mfa: cognito.Mfa.OPTIONAL,
+      mfaSecondFactor: {
+        sms: true,
+        otp: false,
+      },
+
+    });
+
+    // Create the User Groups in the User Pool
+    const userGroup = new cognito.CfnUserPoolGroup(this, 'acManagers', {
+      groupName: 'ac-managers',
+      userPoolId: userPool.userPoolId,
+      description: 'Full access to the art centre',
+    });
+
+    const userGroup1 = new cognito.CfnUserPoolGroup(this, 'acWorkers', {
+      groupName: 'ac-workers',
+      userPoolId: userPool.userPoolId,
+      description: 'Catalogue artworks and products, Manage artists, conduct sales, create consignments',
+    });
+
+    const userGroup2 = new cognito.CfnUserPoolGroup(this, 'admins', {
+      groupName: 'admins',
+      userPoolId: userPool.userPoolId,
+      description: 'Full access to the system',
+    });
+
+    const userGroup3 = new cognito.CfnUserPoolGroup(this, 'artists', {
+      groupName: 'artists',
+      userPoolId: userPool.userPoolId,
+      description: 'Add and edit artwork, Manage the profile information, view money story',
+    });
+
+    const userGroup4 = new cognito.CfnUserPoolGroup(this, 'bookkeepers', {
+      groupName: 'bookkeepers',
+      userPoolId: userPool.userPoolId,
+      description: 'Make payments to artists, manage art centre accounts ',
+    });
+
+    // Set up a domain for the user pool
+    const userPoolDomain = new cognito.UserPoolDomain(this, 'UserPoolDomain', {
+      userPool,
+      cognitoDomain: {
+        domainPrefix: cognitoConfig.domainPrefix
+      }
+    });
+
+    // Setup a Cognito User Pool Client
+    const userPoolClient = new cognito.UserPoolClient(this, 'AppClient', {
+      userPoolClientName: 'sam-website',
+      userPool,
+      authFlows: {
+        adminUserPassword: true,
+        userPassword: true,
+        userSrp: true,
+      },
+      generateSecret: false,
+      preventUserExistenceErrors: true,
+      // OAuth settings
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: true,
+        },
+        scopes: [
+          cognito.OAuthScope.EMAIL,
+          cognito.OAuthScope.OPENID,
+          cognito.OAuthScope.PHONE,
+        ],
+        callbackUrls: [cognitoConfig.callbackUrl],
+        logoutUrls: [cognitoConfig.logoutUrl],
+      },
+
+    });
+
+   // ********************** ElastiCache Redis *****************************
+   const redisConfig = config.ELASTICACHE.REDIS;
+
+   const redisSecurityGroup = new ec2.SecurityGroup(this, redisConfig.security_group_name, {
+     vpc,
+     description: 'Security group for ElastiCache Redis',
+     allowAllOutbound: true,
+   });
+
+   redisSecurityGroup.addIngressRule(ec2.Peer.ipv4(vpcConfigs.vpc_cidr), ec2.Port.tcp(6379), 'Allow Redis traffic');
+
+   const redisSubnetGroup = new elasticache.CfnSubnetGroup(this, redisConfig.subnet_group_name, {
+     description: 'Subnet group for ElastiCache Redis',
+     subnetIds: vpc.privateSubnets.map(subnet => subnet.subnetId),
+     cacheSubnetGroupName: redisConfig.subnet_group_name,
+   });
+
+   new elasticache.CfnCacheCluster(this, redisConfig.cache_cluster_id, {
+     cacheNodeType: redisConfig.node_type,
+     engine: 'redis',
+     numCacheNodes: redisConfig.num_cache_nodes,
+     clusterName: redisConfig.cache_cluster_id,
+     vpcSecurityGroupIds: [redisSecurityGroup.securityGroupId],
+     cacheSubnetGroupName: redisConfig.subnet_group_name,
+     engineVersion: redisConfig.engine_version,
+   });    
 
   }
 }
